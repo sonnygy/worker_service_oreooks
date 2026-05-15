c8f64d8e3a7a488ff8c251f1d6d237ee7dcb895d
Artem Maysky <ijezz@List.ru>
2026-05-15 08:07:17 +0300

    fix

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT: number = Number.parseInt(process.env.PORT as string) ?? 3000;
 
 export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 
 export const ORIOKS_LINK: string = process.env.ORIOKS_LINK || 'https://orioks.miet.ru';
+export const PORT: number = Number.parseInt(process.env.PORT as string) ?? 3000;
 
 if (!DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined in .env');
