8dbee2e85e995118bce7240770cba6d87b5865ed
Artem Maysky <ijezz@List.ru>
2026-05-15 04:30:15 +0300

    db conn fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from "@prisma/client";
 import { DATABASE_URL } from "./env.config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 if (!DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in environment variables');
 }
 
-const prisma = new PrismaClient();
+const pool = new pg.Pool({
+	connectionString: DATABASE_URL
+});
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
